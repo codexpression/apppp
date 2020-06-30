1baff0234ec6980f11c0a39787a8abc1d3e6098a
micheal eze <investormicheal@gmail.com>
2020-06-30 21:49:58 +0100

    Added welcome text and styled it

--- textanimate.js
+++ textanimate.js
@@ -11,3 +11,24 @@ document.querySelector('#freezeButton').addEventListener('click', function(e) {
 
   instance.freeze();
 });
+
+// For welcome text
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i + 1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
